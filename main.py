a1f797116faf017c0fd1fbd14a5dee2e4228dd71
Aprendiz People Analyticse 3 <Aprendizpeoplea3@siju.local>
2026-01-22 20:21:17 -0500

    feat se crea diccionario

---
+++ main.py
@@ -0,0 +1,14 @@
+'''contador=0
+while contador<10:
+        print(f"estoy en la vuelta {contador} ")
+        contador=contador+1'''
+
+
+for o in range(1,6):
+    
+    diccionario={}
+    diccionario["id"]=int(input("ingrese el id del empleado: "))
+    diccionario["nombres"]=input("ingrese los nombres del empleado: ")
+    diccionario["documento"]=input("ingrese el documento del empleado: ")
+    diccionario["correo"]=input("ingrese el correo del empleado: ")
+    diccionario["contraseña"]=input("ingrese la contraseña del empleado: ")
